60c1b98722c5cb9ed65d42a5c02c18614c213cfc
Islom <islomjonalimov83@gmail.com>
2023-03-27 02:15:38 +0500

    toggleSidebar works at togle

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-import "./src/pages/toggleSidebar";
+// global imports
+import "./src/toggleSidebar.js";
+import "./src/src/toggleCart.js";
+import "./src/src/setupCart.js.js";
+
+// specific imports
+import fetchProducts from "./src/fetchProducts.js";
+import { setupStore, store } from "./src/store.js";
+import display from "./src/displayProducts.js";
+import { getElement } from "./src/utils.js";
