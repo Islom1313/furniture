882265cf09b5bccbf4ec79b72ff73f4b1abe70e6
Islom <islomjonalimov83@gmail.com>
2023-03-31 09:36:47 +0500

    insert items into localStorage

--- src/utils.js
+++ src/utils.js
@@ -18,7 +18,9 @@ const getElement = (selection) => {
 const formatPrice = () => {};
 
 const getStorageItem = () => {};
-const setStorageItem = () => {};
+const setStorageItem = (name, item) => {
+  localStorage.setItem(name, JSON.stringify(item));
+};
 
 export {
   allProductsUrl,
