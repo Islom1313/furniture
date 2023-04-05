6575daef90c625f58a0bd0be2d5fd8412b41ef7c
Islom <islomjonalimov83@gmail.com>
2023-04-06 03:04:15 +0500

    price setup complete

--- src/filters/price.js
+++ src/filters/price.js
@@ -16,8 +16,14 @@ const setupPrice = (store) => {
 
   //   event listener
   priceInput.addEventListener("input", () => {
-    const price = parseInt(priceInput.value);
-    priceValue.textContent = `Value: $${price}`;
+    const value = parseInt(priceInput.value);
+    priceValue.textContent = `Value: $${value}`;
+    let newStore = store.filter((product) => product.price / 100 <= value);
+    display(newStore, getElement(".products-container"));
+    if (newStore.length < 1) {
+      const products = getElement(".products-container");
+      products.innerHTML = `<h3 class="filter-error">Sorry, no matches found</h3>`;
+    }
   });
 };
 
