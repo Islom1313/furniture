15d5b7023e05b772566ae046d0fb6dc7d3ffb274
Islom <islomjonalimov83@gmail.com>
2023-04-23 17:38:03 +0500

    project complete

--- src/filters/companies.js
+++ src/filters/companies.js
@@ -23,6 +23,7 @@ const setupCompanies = (store) => {
           (product) => product.company === e.target.textContent
         );
       }
+
       display(newStore, getElement(".products-container"), true);
     }
   });
